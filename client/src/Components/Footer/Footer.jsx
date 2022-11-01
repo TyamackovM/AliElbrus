12f244e9d394ae82ae8967392a6b33d35d520a35
Aleksei Vlasov <aleks101989@mail.ru>
2022-11-01 21:59:22 +0800

    banner,logo

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import {  } from '@ant-design/icons';
 import React from "react";
 
 export default function FooterPage() {
--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -16,30 +16,37 @@ const { Search } = Input;
 // );
 const onSearch = (value) => console.log(value);
 const Navbar = () => (
+  <>
+  <div className={styles.banner_container}>
+  <a href="#" className={styles.banner}></a>
+  </div>
   <Space className={styles.header} direction="vertical">
-   
-    <span>OUR LOGO</span>
+  
+    <div className={styles.logo}>
+      <span className={styles.logo_base}>AliElbrus</span>
+    </div>
     <Search className={styles.input} placeholder="I'm shopping for..." onSearch={onSearch} enterButton />
     <ShoppingCartOutlined className={styles.icon_cart} />
     <div className={styles.cartnum}>
-    <span className={styles.span_number}>0</span>
-    <span>Cart</span>
+      <span className={styles.span_number}>0</span>
+      <span>Cart</span>
     </div>
     <HeartOutlined className={styles.icon_heart} />
     <span>Wish <br /> List</span>
     <UserOutlined className={styles.icon_cart} />
-    <div className={styles.sign_join_my} >
-    <div className={styles.sign_login}>
-    {/* <Link to='/login' className={styles.header_link} >Sign in</Link>
+    <div className={styles.sign_join_my}>
+      <div className={styles.sign_login}>
+        {/* <Link to='/login' className={styles.header_link} >Sign in</Link>
     <span className={styles.devider} > | </span>
     <Link to='/registr' className={styles.header_link}>Join</Link> */}
-    <ModalPage />
-    </div>
-    <div>
-    <span>My AliElbrus</span>
-    </div>
+        <ModalPage />
+      </div>
+      <div>
+        <span>My AliElbrus</span>
+      </div>
     </div>
 
   </Space>
+  </>
 );
 export default Navbar;
