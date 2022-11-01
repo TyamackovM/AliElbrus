6c26dd187051cccf1be1f6d143a1fdd3b681789e
DanilaTru <dan.trurk@gmail.com>
2022-11-01 11:32:38 +0300

    footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
+import Main from "./Components/Main/Main";
+
+
 function App() {
   return (
-    <div>APP</div>
+    <div>APP
+      <Main></Main>
+    </div>
   );
 }
 
--- client/src/Components/Main/Main.jsx
+++ client/src/Components/Main/Main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Footer from '../Footer/Footer'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div>Main
+      <Footer></Footer>
+    </div>
   )
 }
