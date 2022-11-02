7e22e7d1ab5d8814f7a1735529c829028a39b202
StepanMogilyov <st.mogilev@mail.ru>
2022-11-02 16:48:59 +0300

    reder-cards v1

--- client/src/Components/AllCards/AllCards.jsx
+++ client/src/Components/AllCards/AllCards.jsx
@@ -6,43 +6,16 @@ import {
 import styles from './AllCards.module.css';
 import { Avatar, Card } from 'antd';
 import React from 'react';
+import OneCard from './OneCard';
 const { Meta } = Card;
 
 export default function AllCards({ allItems }) {
 
-
-  const selectCardHandler = (event) => {
-    console.log(event);
-
-  }
-
   return (
     <div className={styles.mainDiv}>
-      {allItems?.map((el) => {
-        console.log('el: ', el);
-        return (
-          <Card key={el["Items.id"]}
 
-            onClick={selectCardHandler}
+      {allItems?.map((el) => <OneCard el={el} key={el['Items.id']} />)}
 
-            style={{
-              width: 300,
-            }}
-            cover={<img alt="123123123123" src={el["Items.image"]} />}
-            // actions={[
-            //   <SettingOutlined key="setting" />,
-            //   <EditOutlined key="edit" />,
-            //   <EllipsisOutlined key="ellipsis" />,
-            // ]}
-          >
-            <Meta
-              avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-              title={'$' + el["Items.price"]}
-              description="This is the description"
-            />
-          </Card>
-        );
-      })}
     </div>
   );
 }
