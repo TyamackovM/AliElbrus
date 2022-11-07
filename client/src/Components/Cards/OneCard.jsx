cb13e447c195a9615e2c3a988812f8fe2a79a59e
Aleksei Vlasov <aleks101989@mail.ru>
2022-11-07 17:44:57 +0800

    wishlist 0.5

--- client/src/Components/Cards/OneCard.jsx
+++ client/src/Components/Cards/OneCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./OneCard.module.css";
+import { useSelector, useDispatch } from "react-redux";
 import {
   EditOutlined,
   HeartOutlined,
@@ -8,41 +9,86 @@ import {
   ShoppingCartOutlined,
 } from "@ant-design/icons";
 import { Avatar, Card } from "antd";
+import { useState } from "react";
 const { Meta } = Card;
+
 export default function OneCard({ el }) {
-  const selectCardHandler = (event) => {
-    // console.log(event);
+  const user_id = useSelector((state) => state.user.id);
+  const [likeFill, setLikeFill] = useState(false);
+
+  const selectCardHandler = async (event) => {
+    event.preventDefault();
+
+    const item_id = +event.target.parentNode.parentNode.id;
+
+    if (
+      !likeFill &&
+      event.target.tagName === "svg" &&
+      event.target.parentNode.parentNode.tagName === "BUTTON"
+    ) {
+      setLikeFill(!likeFill);
+      const response = await fetch(
+        "http://localhost:4000/add-item-to-wish-list",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ user_id, item_id }),
+        }
+      );
+    } else {
+      setLikeFill(!likeFill);
+    }
   };
+
   return (
     <Card
       key={el.id}
       onClick={selectCardHandler}
       style={{
-
-        width: '200px',
-        height: '347px',
-        borderRadius: '5px',
-        boxShadow: "1px 1px 1px 1px rgba(167, 167, 167, 0.596)"
-
+        width: "200px",
+        height: "347px",
+        borderRadius: "5px",
+        boxShadow: "1px 1px 1px 1px rgba(167, 167, 167, 0.596)",
       }}
       cover={
         <img
           className={styles.image}
           alt="Items_image"
-          style={{ borderRadius: "5px 5px 0px 0px" }}
+          style={{
+            borderRadius: "5px 5px 0px 0px",
+            width: "200px",
+            height: "260px",
+          }}
           src={el.image}
         />
       }
-      // actions={[
-      //  <EllipsisOutlined key="ellipsis" />,
-      // ]}
     >
       <div className={styles.card_bottom}>
         <span className={styles.price}>{"$" + el.price}</span>
-        <div className={styles.heart}>
-          <HeartOutlined className={styles.icon_card_heart} />
-        </div>
+
         <ShoppingCartOutlined style={{ fontSize: "22px", color: "grey" }} />
+      </div>
+      <div name="heart" className={styles.heart}>
+        <button
+          id={el.id}
+          style={{
+            border: "0px",
+            background: "none",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          {likeFill ? (
+            <HeartOutlined
+              style={{ color: "red" }}
+              className={styles.icon_card_heart}
+            />
+          ) : (
+            <HeartOutlined className={styles.icon_card_heart} />
+          )}
+        </button>
       </div>
     </Card>
   );
