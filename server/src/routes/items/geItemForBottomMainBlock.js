bc4d6879b1a910ed536b09587719e1971861df2e
StepanMogilyov <st.mogilev@mail.ru>
2022-11-09 16:12:42 +0300

    render randomcards in prog

--- server/src/routes/items/geItemForBottomMainBlock.js
+++ server/src/routes/items/geItemForBottomMainBlock.js
@@ -1,14 +1,51 @@
 const router = require('express').Router();
 // const { Category } = require('../../../db/models');
-const { Item } = require('../../../db/models');
+const { Item, Sequelize } = require('../../../db/models');
 
+// function getShuffledArray(arr) {
+//   const newArr = arr.slice()
+//   for (let i = newArr.length - 1; i > 0; i--) {
+//     const rand = Math.floor(Math.random() * (i + 1));
+//     [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
+//   }
+//   return newArr
+// };
+
+// router.get('/', async (req, res) => {
+//   try {
+//     const getDresses = await Item.findAll({ where: { category_id: 1 }, raw: true });
+//     if(getDresses) {
+//       res.json(getShuffledArray(getDresses))
+//     } 
+//   } catch (error) {
+//     console.log('error: ', error);    
+//   }
+// });
+
+// module.exports = router;
+
+function getRandomCategory() {
+  const categoriesQuantity = 26
+  const getRandomCategory = Math.floor(Math.random() * (26 - 1) + 1)
+  return getRandomCategory
+}
 router.get('/', async (req, res) => {
-  try {
-    const getFields = await Item.findAll({ where: { category_id: 1 }, raw: true });    
-    res.json(getFields);
-  } catch (error) {
-    console.log('error: ', error);    
+  let counter = 0
+  const result = []
+  while(counter <= 5) {
+    counter++
+    const res = await Item.findAll({ where: { category_id: getRandomCategory() }, raw: true });
+    // result.push(getRandomCategory())
+    console.log('res: ', res);
   }
+  // try {
+  //   const getDresses = await Item.findAll({ where: { category_id: 1 }, raw: true });
+  //   if(getDresses) {
+  //     res.json(getShuffledArray(getDresses))
+  //   } 
+  // } catch (error) {
+  //   console.log('error: ', error);    
+  // }
 });
 
 module.exports = router;
