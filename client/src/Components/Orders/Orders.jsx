2ab1048670fe6cc6b5c85a08c23e95df6c942639
DanilaTru <dan.trurk@gmail.com>
2022-11-09 16:16:18 +0300

    item-add-cart 0.7

--- client/src/Components/Orders/Orders.jsx
+++ client/src/Components/Orders/Orders.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import OrderMessage from './OrderMessage'
+
 
 export default function Orders() {
   return (
-    <div>Orders</div>
+    <div>
+    <OrderMessage/>
+    </div>
   )
 }
