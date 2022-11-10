8b999aa78df478623d03a6fa7e92dad783e85ae3
StepanMogilyov <st.mogilev@mail.ru>
2022-11-10 15:40:13 +0300

    deploy in prog

--- server/src/app.js
+++ server/src/app.js
@@ -18,26 +18,30 @@ const editEmail = require('./routes/user/editEmail');
 const itemsToSliderRouter = require('./routes/items/itemsToSliderRouter');
 const findItemFromInput = require('./routes/items/findItemFromInput');
 const addItemToWishList = require('./routes/user/addItemToWishList');
-const deleteWishList = require('./routes/user/deleteWishList')
+const deleteWishList = require('./routes/user/deleteWishList');
 const findItemCategoryFilter = require('./routes/items/findItemCategoryFilter');
 const getItemsFieldsForAdmin = require('./routes/items/getItemsFieldsForAdmin');
-const displayWishList = require('./routes/user/displayWishList')
+const displayWishList = require('./routes/user/displayWishList');
 const loadItemPagination = require('./routes/items/loadItempagination');
 const loadItemFilterPagination = require('./routes/items/loadItemFilterPagination');
-const addItemToCart = require('./routes/user/addItemToCart')
-const displayCart = require('./routes/user/displayCart')
-const deleteItemFromCart = require('./routes/user/deleteItemFromCart')
-const deleteItemFromWishListCart = require('./routes/user/deleteItemFromWishListCart')
-const addNewItem = require('./routes/items/addNewItem')
-const searchUserByEmail = require('./routes/user/searchUserByEmail')
-const updateUserStatus = require('./routes/user/updateStatus')
-const geItemForBottomMainBlock = require('./routes/items/geItemForBottomMainBlock')
-const createOrder = require('./routes/user/createOrder')
-const checkOneItem = require('./routes/items/checkOneItem')
-const addManyItemToCart = require('./routes/items/add-many-item-to-cart')
-const displayOrders = require('./routes/user/displayOrders')
+const addItemToCart = require('./routes/user/addItemToCart');
+const displayCart = require('./routes/user/displayCart');
+const deleteItemFromCart = require('./routes/user/deleteItemFromCart');
+const deleteItemFromWishListCart = require('./routes/user/deleteItemFromWishListCart');
+const addNewItem = require('./routes/items/addNewItem');
+const searchUserByEmail = require('./routes/user/searchUserByEmail');
+const updateUserStatus = require('./routes/user/updateStatus');
+const geItemForBottomMainBlock = require('./routes/items/geItemForBottomMainBlock');
+const createOrder = require('./routes/user/createOrder');
+const checkOneItem = require('./routes/items/checkOneItem');
+const addManyItemToCart = require('./routes/items/add-many-item-to-cart');
+const displayOrders = require('./routes/user/displayOrders');
 
+app.use(express.static(path.join(__dirname, '../client/build')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('../client/build/index.html'));
+});
 
 const app = express();
 
@@ -74,25 +78,25 @@ app.use('/edit-email', editEmail);
 app.use('/find-slider', itemsToSliderRouter);
 app.use('/check-item', findItemFromInput);
 app.use('/add-item-to-wish-list', addItemToWishList);
-app.use('/delete-item-from-wish-list', deleteWishList)
+app.use('/delete-item-from-wish-list', deleteWishList);
 app.use('/filter-category', findItemCategoryFilter);
-app.use('/get-fields', getItemsFieldsForAdmin)
-app.use('/display-wishlist', displayWishList)
+app.use('/get-fields', getItemsFieldsForAdmin);
+app.use('/display-wishlist', displayWishList);
 app.use('/get-next-page', loadItemPagination);
 app.use('/get-next-filter-page', loadItemFilterPagination);
 app.use('/add-item-to-cart', addItemToCart);
-app.use('/delete-item-from-cart', deleteItemFromCart)
-app.use('/delete-item-from-wish-list-cart', deleteItemFromWishListCart)
-app.use('/display-cart', displayCart)
-app.use('/add-new-item', addNewItem)
-app.use('/search-by-email', searchUserByEmail)
-app.use('/update-status', updateUserStatus)
-app.use('/create-order', createOrder)
-app.use('/update-status', updateUserStatus)
-app.use('/check-one-item', checkOneItem)
-app.use('/add-many-item-to-cart', addManyItemToCart)
-app.use('/get-item-main-bottom', geItemForBottomMainBlock)
-app.use('/display-orders', displayOrders)
+app.use('/delete-item-from-cart', deleteItemFromCart);
+app.use('/delete-item-from-wish-list-cart', deleteItemFromWishListCart);
+app.use('/display-cart', displayCart);
+app.use('/add-new-item', addNewItem);
+app.use('/search-by-email', searchUserByEmail);
+app.use('/update-status', updateUserStatus);
+app.use('/create-order', createOrder);
+app.use('/update-status', updateUserStatus);
+app.use('/check-one-item', checkOneItem);
+app.use('/add-many-item-to-cart', addManyItemToCart);
+app.use('/get-item-main-bottom', geItemForBottomMainBlock);
+app.use('/display-orders', displayOrders);
 
 const httpServer = app.listen(CURRENT_PORT ?? 5000, () => {
   console.log(`Server started ${CURRENT_PORT}`);
@@ -108,4 +112,3 @@ wsServer.on('connection', (currentClient) => {
     });
   });
 });
-
